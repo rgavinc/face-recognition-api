e36c653b2994321d971730fabdd039e1492f88a7
Gavin Robertson <gavinrob92@gmail.com>
2018-12-17 20:30:52 -0700

    added cors, bcrypt, small changes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const uuidv4 = require("uuid/v4");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
   users: [
@@ -43,10 +46,18 @@ app.put("/image", (req, res) => {
   const user = database.users.find(user => user.id === id);
   if (user) {
     user.entries++;
-    res.json(user);
+    res.json(user.entries);
   }
   res.status(404).json("error getting user");
 });
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//   // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//   // res = false
+// });
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
@@ -55,23 +66,23 @@ app.listen(3000, () => {
 app.post("/signin", (req, res) => {
   const users = database.users;
 
-  if (
-    users.some(
-      user =>
-        req.body.email === user.email && req.body.password === user.password
-    )
-  )
-    res.json("success");
-  else res.status(404).json("error loggin in");
+  const user = users.find(
+    user => req.body.email === user.email && req.body.password === user.password
+  );
+  if (user) res.json(user);
+  else res.status(400).json("error loggin in");
 });
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
+  bcrypt.hash(password, null, null, function(err, hash) {
+    console.log({ hash });
+  });
+
   database.users.push({
     id: uuidv4(),
     name,
     email,
-    password,
     entries: 0,
     joined: new Date()
   });
